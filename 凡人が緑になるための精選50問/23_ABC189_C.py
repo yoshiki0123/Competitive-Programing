8f366ccfaa6b593f3981727539290e5ccfa3ab61
yoshiki0123 <yoshiki.tamehira1212@gmail.com>
2025-04-22 23:43:01 +0900

    19日目

--- 凡人が緑になるための精選50問/23_ABC189_C.py
+++ 凡人が緑になるための精選50問/23_ABC189_C.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #library
 import sys, re
 from math import ceil, floor, sqrt, pi, gcd
@@ -28,35 +27,4 @@ def main():
     print(ans)
 
 if __name__ == '__main__':
-=======
-#library
-import sys, re
-from math import ceil, floor, sqrt, pi, gcd
-from collections import deque, Counter
-from bisect import bisect, bisect_left, bisect_right
-from functools import lru_cache
-from itertools import permutations, combinations
-
-sys.setrecursionlimit(10**6)
-INF = float('inf')
-
-#def
-
-#main
-def main():
-    N = int(input())
-    A = list(map(int, input().split()))
-
-    ans = -10**15
-
-    for l in range(N):
-        A_min = A[l]
-        for r in range(l, N):
-            A_min = min(A_min, A[r])
-            ans = max(ans, A_min * (r - l + 1))
-    
-    print(ans)
-
-if __name__ == '__main__':
->>>>>>> d79bd24fdd6ac0f676cacb0493d46b4acf8ea1ef
     main()
