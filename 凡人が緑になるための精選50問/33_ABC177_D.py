d847ed9b51a840d463b4271e4583c9f068df29f6
yoshiki0123 <yoshiki.tamehira1212@gmail.com>
2025-05-01 20:21:05 +0900

    28日目

---
+++ 凡人が緑になるための精選50問/33_ABC177_D.py
@@ -0,0 +1,68 @@
+#library
+import sys, re
+from math import ceil, floor, sqrt, isqrt, pi, gcd, comb
+from collections import deque, Counter
+from bisect import bisect, bisect_left, bisect_right
+from functools import lru_cache
+from itertools import permutations, combinations
+
+sys.setrecursionlimit(10**6)
+INF = float('inf')
+
+#class
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        self.parent_size = [-1] * n
+
+    def leader(self, a):
+        if self.parent_size[a] < 0:
+            return a
+        self.parent_size[a] = self.leader(self.parent_size[a])
+        return self.parent_size[a]
+
+    def merge(self, a, b):
+        x, y = self.leader(a), self.leader(b)
+        if x == y:
+            return
+        if abs(self.parent_size[x]) < abs(self.parent_size[y]):
+            x, y = y, x
+        self.parent_size[x] += self.parent_size[y]
+        self.parent_size[y] = x
+
+    def same(self, a, b):
+        return self.leader(a) == self.leader(b)
+
+    def size(self, a):
+        return abs(self.parent_size[self.leader(a)])
+
+    def groups(self):
+        result = [[] for _ in range(self.n)]
+        for i in range(self.n):
+            result[self.leader(i)].append(i)
+        return [r for r in result if r]
+
+#def
+
+#main
+def main():
+    N, M = map(int,input().split())
+    Uni = UnionFind(N)
+    
+    for i in range(M):
+        A, B = map(int, input().split())
+        A -= 1
+        B -= 1
+        Uni.merge(A, B)
+    
+    friends_group = Uni.groups()
+    friends_size = []
+
+    for fri in friends_group:
+        friends_size.append(len(fri))
+    
+    print(max(friends_size))
+
+
+if __name__ == '__main__':
+    main()
