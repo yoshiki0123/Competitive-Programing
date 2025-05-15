616f653178c3499b0dfcf027f21c2a352540df20
yoshiki0123 <yoshiki.tamehira1212@gmail.com>
2025-05-16 00:07:05 +0900

    42日目

--- 凡人が緑になるための精選50問/42_DPコンテストまとめ_D.py
+++ 凡人が緑になるための精選50問/42_DPコンテストまとめ_D.py
@@ -16,7 +16,22 @@ INF = float('inf')
 
 #main
 def main():
+    N, W = map(int,input().split())
+    items = [(0, 0)]
+    for _ in range(N):
+        w, v = map(int,input().split())
+        items.append((w, v))
+    dp = [[0]*(W+1) for _ in range(N+1)]
     
-
+    for i in range(1, N+1):
+        w, v = items[i]
+        for j in range(W+1):
+            if 0 <= i-1 <= N-1 and 0 <= j-w <= W:
+                dp[i][j] = max(dp[i-1][j-w] + v, dp[i-1][j])
+            else:
+                dp[i][j] = dp[i-1][j]
+    
+    print(dp[N][W])
+    
 if __name__ == '__main__':
     main()
