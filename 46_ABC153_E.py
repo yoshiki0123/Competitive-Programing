2a6dd71b1f8f4536538c9eac8ce5272cc606c835
yoshiki0123 <yoshiki.tamehira1212@gmail.com>
2025-05-22 20:05:37 +0900

    47日目

--- 46_ABC153_E.py
+++ 46_ABC153_E.py
@@ -1,25 +1,21 @@
 #library
-import sys, re
-from math import ceil, floor, sqrt, pi, gcd
+import sys, re, heapq
+from math import sin, cos, radians, ceil, floor, sqrt, isqrt, pi, gcd, comb
 from collections import deque, Counter
 from bisect import bisect, bisect_left, bisect_right
 from functools import lru_cache
-from itertools import permutations, combinations
+from itertools import permutations, combinations, product
 
 sys.setrecursionlimit(10**6)
 INF = float('inf')
+
+#class
 
 #def
 
 #main
 def main():
-    N = int(input())
-    rice_cake_kinds = set()
-    for _ in range(N):
-        rice_cake_kinds.add(int(input()))
     
-    print(len(rice_cake_kinds))
-
 
 if __name__ == '__main__':
     main()
