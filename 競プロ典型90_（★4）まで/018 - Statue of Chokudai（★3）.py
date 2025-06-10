4e11e371931737aa8b79545a20ed863cf8b276c1
yoshiki0123 <yoshiki.tamehira1212@gmail.com>
2025-06-10 18:29:24 +0900

    61日目

---
+++ 競プロ典型90_（★4）まで/018 - Statue of Chokudai（★3）.py
@@ -0,0 +1,21 @@
+#library
+import sys, re, heapq
+from math import sin, cos, radians, ceil, floor, sqrt, isqrt, pi, gcd, comb
+from collections import deque, Counter, defaultdict
+from bisect import bisect, bisect_left, bisect_right
+from functools import lru_cache
+from itertools import permutations, combinations, product
+
+sys.setrecursionlimit(10**6)
+INF = float('inf')
+
+#class
+
+#def
+
+#main
+def main():
+    
+
+if __name__ == '__main__':
+    main()
